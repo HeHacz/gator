689110f38d1d7daf6d29a9e4049cbeb09b7f8f2f
hehacz <hehahiyourip@gmail.com>
2025-07-21 22:29:44 +0200

    added unfollow function

--- handler_feed_follows.go
+++ handler_feed_follows.go
@@ -45,6 +45,26 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 	return nil
 }
 
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf(("unfollow cmd expects one argument, the feed URL"))
+	}
+	feed, err := s.db.GetFeedbyURL(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("coudnt find feed with url %s error: %v", cmd.args[0], err)
+	}
+	payload := database.DeleteFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	}
+	err = s.db.DeleteFollow(context.Background(), payload)
+	if err != nil {
+		return fmt.Errorf("coudnt delete follow for a user %s, with url: %s error: %v", user.Name, feed.Url, err)
+	}
+	fmt.Printf("Follow for a user %s, with url: %s has been successfuly removed", user.Name, feed.Url)
+	return nil
+}
+
 func printFeedFollow(username, feedname string) {
 	fmt.Printf("* User:		%s\n", username)
 	fmt.Printf("* Feed: 	%s\n", feedname)
